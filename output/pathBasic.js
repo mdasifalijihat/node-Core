1d8c08d4220ea61c42281645e0ab0b2d9fff0608
mdasifalijihat <mdasifalijihat@gmail.com>
2025-11-22 12:00:30 +0600

    added path basic

---
+++ output/pathBasic.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+console.log("Current file Info: \n");
+
+console.log("filename:", __filename);
+console.log("Directory:", __dirname);
+
+console.log("\n" + "-".repeat(50) + "\n");
+
+const filePath = "/Asif/document/nextLevel.pdf";
+
+console.log("analyzing Path:", filePath, "\n");
+console.log('Directory:', path.dirname(filePath));
+console.log("Base name:", path.basename(filePath));
+console.log("File extension: ", path.extname(filePath));
+console.log("file name:", path.basename(filePath, path.extname(filePath)))
+
+console.log("\n" + "-".repeat(50) + "\n");
+
+const parsed = path.parse(filePath);
+console.log("Parsed path object:", parsed)
+console.log("\n" + "-".repeat(50) + "\n");
+console.log("formatted path:" , path.format(parsed))
+console.log("\n" + "-".repeat(50) + "\n");
